e27c4baaac61956ff6da0d63aee075da698df8f6
jinlong <arowana.java@qq.com>
2019-02-15 10:40:53 +0800

    :tada:创建项目子模块,并支持maven deploy 在github中

---
+++ horologium-core/src/test/java/com/aries/time/horologium/core/AppTest.java
@@ -0,0 +1,18 @@
+package com.aries.time.horologium.core;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
