cf6043d18dbfe40904d5fba0c258fe0b9f40acc7
jinlong <arowana.java@qq.com>
2019-02-24 15:50:03 +0800

    :sparkles: 增加jdk8 Year类测试

--- jdk8-time/src/test/java/com/aries/time/jdk8/YearTest.java
+++ jdk8-time/src/test/java/com/aries/time/jdk8/YearTest.java
@@ -6,13 +6,17 @@ import java.time.Year;
 
 public class YearTest {
     @Test
-    public void nowTest(){
+    public void nowTest() {
         Year year = Year.now();
         System.out.println(year);
     }
 
     @Test
-    public void n(){
+    public void n() {
         Year.isLeap(2019);
+//        Year.from(;
+        Year.of(1990).getValue();
+        Year.parse();
+
     }
 }
